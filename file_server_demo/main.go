50862d1b9999578b1328a12eea5bcd191924d51f
hulksmasher <somesh.08.96@gmail.com>
2018-11-20 23:32:30 +0530

    file server demo

---
+++ file_server_demo/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "io"
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		f, err := os.Open("public" + r.URL.Path)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Println(err)
+		}
+		defer f.Close()
+		io.Copy(w, f)
+	})
+	http.ListenAndServe(":8000", nil)
+}
